74d462b1f58ed7b3db92abaf7ff36313f060ec15
emma <emmasofia@live.fi>
2024-01-06 22:15:14 +0200

    4.5

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -6,10 +6,20 @@ const totalLikes = (blogs) => {
     return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 
 }
+
+const favoriteBlog = (blogs) => {
+    const maxLikes = blogs.sort(function(a, b) {return b.likes-a.likes})
+    return {
+        title: maxLikes[0].title,
+        author: maxLikes[0].author,
+        likes: maxLikes[0].likes
+    }
+}
   
   module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
   }
 
 
